ead3ff5cd494c4e2062ace6b799de6031711c135
marx <2443005004@qq.com>
2022-10-28 01:30:22 +0800

    use esm specifications

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,14 @@
-const path = require("path");
+import { terser } from "rollup-plugin-terser";
+import ts from "rollup-plugin-typescript2";
+import commonjs from "rollup-plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
 
-const resolve = require("@rollup/plugin-node-resolve");
-const { terser } = require("rollup-plugin-terser");
-const ts = require("rollup-plugin-typescript2");
-const commonjs = require('rollup-plugin-commonjs');
-
-module.exports = {
+const config = {
   input: "src/lib/index.ts", // 打包入口
   output: [
     {
       file: "dist/index.js", // 输出的文件
-       format: "cjs", // 文件模块规范
+      format: "cjs", // 文件模块规范
     },
   ],
   plugins: [
@@ -18,10 +16,13 @@ module.exports = {
     // 打包插件
     resolve(), // rollup导入语法只能导入本地文件，这里用于转换语法，告诉导入的是第三方库
     terser(), // 压缩代码和去除注释
-    ts({ // 解析 typescript
-      tsconfig: path.resolve(__dirname, "tsconfig.rollup.json"),
+    ts({
+      // 解析 typescript
+      tsconfig: "tsconfig.rollup.json",
       extensions: [".js", ".ts", ".tsx"], // 解析的扩展名
     }),
   ],
-  external: ['react', 'react-dom'], // 声明使用的第三方外部模块，要不然打包就会报错
+  external: ["react", "react-dom"], // 声明使用的第三方外部模块，要不然打包就会报错
 };
+
+export default config;
